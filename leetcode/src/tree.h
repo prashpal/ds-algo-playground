f1e4f4fc78826070c8674537f85e839c467e429b
Prashanth Palaniappan <prashanthrulz@gmail.com>
2025-07-16 11:08:45 -0500

    optimize tree max depth solution

--- leetcode/src/tree.h
+++ leetcode/src/tree.h
@@ -18,8 +18,6 @@ public:
     int maxDepth(TreeNode* root) {
         if (root == nullptr) return 0;
         else if (root->left == nullptr && root->right == nullptr) return 1;
-        else if (root->left != nullptr && root->right == nullptr) return (1 + maxDepth(root->left));
-        else if (root->left == nullptr && root->right != nullptr) return (1 + maxDepth(root->right)); 
         else return (1 + std::max(maxDepth(root->left), maxDepth(root->right)));
     }
 };
