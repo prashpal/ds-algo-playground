93f3ecd6847a091d5546c0c3baad7a65eb9f11d0
Prashanth Palaniappan <prashanthrulz@gmail.com>
2025-07-16 11:07:20 -0500

    add unit test for leetcode 104

--- leetcode/src/tree_test.cpp
+++ leetcode/src/tree_test.cpp
@@ -1,4 +1,33 @@
 #include <gtest/gtest.h>
 #include "tree.h"
 
+TreeNode* createTree(const std::vector<int>& values) {
+    if (values.empty()) return nullptr;
+    std::vector<TreeNode*> nodes;
+    for (int val : values) {
+        nodes.push_back(val == -1 ? nullptr : new TreeNode(val));
+    }
+    for (size_t i = 0, j = 1; j < nodes.size(); ++i) {
+        if (nodes[i]) {
+            if (j < nodes.size()) nodes[i]->left = nodes[j++];
+            if (j < nodes.size()) nodes[i]->right = nodes[j++];
+        }
+    }
+    return nodes[0];
+}
+
+TEST(Tree, MaxDepth) {
+    BinaryTree tree;
+    TreeNode* root = createTree({1, 2, 3, 4, 5, -1, -1});
+    EXPECT_EQ(tree.maxDepth(root), 3);
+
+    root = createTree({1, 2, -1, 3});
+    EXPECT_EQ(tree.maxDepth(root), 3);
+
+    root = createTree({});
+    EXPECT_EQ(tree.maxDepth(root), 0);
+
+    root = createTree({1});
+    EXPECT_EQ(tree.maxDepth(root), 1);
+}
 
